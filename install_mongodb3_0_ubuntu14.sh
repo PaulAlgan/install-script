cdf32e2daa888be92b578d20f72f03fcd12dfa01
phatthana <kndy8@hotmail.com>
2016-02-11 21:28:58 +0700

    update script

--- install_mongodb3_0_ubuntu14.sh
+++ install_mongodb3_0_ubuntu14.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "export LC_ALL=C" >> .profile
+export LC_ALL=C"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
